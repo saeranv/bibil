93609d3904db07eb20464665927725668e8b5c84
Saeran Vasanthakumar <saeranv@gmail.com>
2016-08-13 14:04:17 -0400

    Working on standard form line code.

---
+++ src/koku/koku_standard_form.py
@@ -0,0 +1,52 @@
+"""
+Playing around with standard form of a line Ax + By = C
+"""
+
+import rhinoscriptsyntax as rs
+
+A = 10
+B = 12
+C = 500
+
+L = []
+# Standard form of this line is
+# Ax + By = C
+# 10x + 12y = 500
+# Any x,y that is solution to this is on the line
+
+for i in range(100):
+    x = i
+    y = (C - A*x)/B
+    L.append(rs.AddPoint(x,y,0))
+    print 'x', x, 'y', y
+
+
+## Question: convert standard form
+## to parametric form by finding the 
+## base points (A,B is not 0], and dir vector by moving
+## line to oriign.
+
+"""
+If we move Ax + By = C to the origin point, then 
+any point x,y is a direction vector for the line. 
+
+Therefore, the normal to the line is [B,-A].
+That is how you fin dthe direction vector and normal from
+standard form.
+
+"""
+
+
+"""
+A_ = 12
+B_ = -10
+C_ = 500
+
+
+for i in range(100):
+    x = i
+    y = (C_ - A_*x)/B_
+    L.append(rs.AddPoint(x,y,0))
+    print x,y
+"""
+o = L
