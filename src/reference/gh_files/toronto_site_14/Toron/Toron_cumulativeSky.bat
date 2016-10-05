294a53fef4db6036583c29bc02cc0fa75e60523e
Saeran Vasanthakumar <saeranv@gmail.com>
2016-10-04 23:23:20 -0400

    Deleting idf files, and reverting commits.

--- src/reference/gh_files/toronto_site_14/Toron/Toron_cumulativeSky.bat
+++
@@ -1,4 +0,0 @@
-C:
-cd C:\Users\vasanthakumars\AppData\Roaming\Ladybug\skylib\cumulativeSkies\Toron
-C:\Users\vasanthakumars\AppData\Roaming\Ladybug\\GenCumulativeSky +s1 -a 43.67 -o 79.63 -m 75.0 -p -E -time 1 24 -date 1 1 12 31 C:\Users\vasanthakumars\AppData\Roaming\Ladybug\skylib\cumulativeSkies\Toron\Toron.epw 1> C:\Users\vasanthakumars\AppData\Roaming\Ladybug\skylib\cumulativeSkies\Toron\Toron_1.cal 2> C:\Users\vasanthakumars\AppData\Roaming\Ladybug\skylib\cumulativeSkies\Toron\Toron_1_report.txt
-cd\
