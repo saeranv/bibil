a1a3db3641a7053d2b28bc38f674403628db26a2
Saeran Vasanthakumar <saeranv@gmail.com>
2016-10-11 02:39:20 -0400

    Starting to code rref

--- src/koku/koku_linear_system.py
+++ src/koku/koku_linear_system.py
@@ -79,8 +79,18 @@ class LinearSystem(object):
                     continue
                 else:
                     raise e
-
         return indices
+    def compute_triangular_form(self):
+        #RREF:
+        #1. rows with all zeros are below nonzero rows
+        #2. leading coefficients (pivots) are left of leading
+        #   coefficients below them. And are 1.
+        #3. Coefficients below pivots (leading) coefficients = zero
+        #4. If RREF acheived, then one variable is a parameter
+        #   find the parameter, and define it as such
+        system = deepcopy(self)
+        return system
+    
     def __len__(self):
         return len(self.planes)
     def __getitem__(self, i):
@@ -107,6 +117,45 @@ class MyDecimal(Decimal):
         return abs(float(self)) < eps
 
 
+##Test 3
+p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['0','1','1']), constant_term='2')
+s = LinearSystem([p1,p2])
+t = s.compute_triangular_form()
+if not (t[0] == p1 and
+        t[1] == p2):
+    print 'test case 1 failed'
+"""
+p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['1','1','1']), constant_term='2')
+s = LinearSystem([p1,p2])
+t = s.compute_triangular_form()
+if not (t[0] == p1 and
+        t[1] == Plane(constant_term='1')):
+    print 'test case 2 failed'
+
+p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['0','1','0']), constant_term='2')
+p3 = Plane(normal_vector=Vector(['1','1','-1']), constant_term='3')
+p4 = Plane(normal_vector=Vector(['1','0','-2']), constant_term='2')
+s = LinearSystem([p1,p2,p3,p4])
+t = s.compute_triangular_form()
+if not (t[0] == p1 and
+        t[1] == p2 and
+        t[2] == Plane(normal_vector=Vector(['0','0','-2']), constant_term='2') and
+        t[3] == Plane()):
+    print 'test case 3 failed'
+
+p1 = Plane(normal_vector=Vector(['0','1','1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['1','-1','1']), constant_term='2')
+p3 = Plane(normal_vector=Vector(['1','2','-5']), constant_term='3')
+s = LinearSystem([p1,p2,p3])
+t = s.compute_triangular_form()
+if not (t[0] == Plane(normal_vector=Vector(['1','-1','1']), constant_term='2') and
+        t[1] == Plane(normal_vector=Vector(['0','1','1']), constant_term='1') and
+        t[2] == Plane(normal_vector=Vector(['0','0','-9']), constant_term='-2')):
+    print 'test case 4 failed'
+"""
 
 ## Test 0
 """
@@ -129,7 +178,7 @@ print MyDecimal('1e-9').is_near_zero()
 print MyDecimal('1e-11').is_near_zero()
 """
 ## Test 2
-
+"""
 p0 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
 p1 = Plane(normal_vector=Vector(['0','1','0']), constant_term='2')
 p2 = Plane(normal_vector=Vector(['1','1','-1']), constant_term='3')
@@ -192,4 +241,6 @@ if not (s[0] == Plane(normal_vector=Vector(['-10','-10','-10']), constant_term='
         s[2] == Plane(normal_vector=Vector(['-1','-1','1']), constant_term='-3') and
         s[3] == p3):
     print 'test case 9 failed'
-    
+
+"""
+
