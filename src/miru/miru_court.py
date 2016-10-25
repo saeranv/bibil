0e88b1afd6a2a8dc88286b5a765c1a68db19827d
Saeran Vasanthakumar <saeranv@gmail.com>
2016-10-25 17:14:12 -0400

    Adding geometries from test case: Montgomery Square

--- src/miru/miru_court.py
+++ src/miru/miru_court.py
@@ -11,9 +11,9 @@ from Grasshopper.Kernel.Data import GH_Path
 from Grasshopper import DataTree
  
 if run:   
-    if parent_ref:
-        if type(parent_ref) != type(int(1)):
-            parent_ref = rs.coercecurve(parent_ref)
+    if court_ref:
+        if type(court_ref) != type(int(1)):
+            court_ref = rs.coercecurve(court_ref)
     else:
         parent_ref = 1
         
@@ -22,7 +22,7 @@ if run:
     ['court_width', podium_depth],
     ['court_node', -1],
     ['court_slice', True],
-    ['court_ref', parent_ref]]
+    ['court_ref', court_ref]]
 else:
     rule = []
     
--- src/miru/miru_stepback.py
+++ src/miru/miru_stepback.py
@@ -4,14 +4,23 @@ Miru Stepback
 import copy
 import scriptcontext as sc
 import rhinoscriptsyntax as rs
-#Miru = sc.sticky["Miru"]
-#R = copy.deepcopy(Miru)
+import clr
+
+clr.AddReference("Grasshopper")
+from Grasshopper.Kernel.Data import GH_Path
+from Grasshopper import DataTree
+
 
 if run:
-    rule = [\
+    rule = DataTree[object]()
+    rule_ = [\
     ['stepback', True],\
     ['stepback_node', node_ref],\
     ['stepback_data', map(lambda x,y: (x,y),height,distance)],\
-    ['stepback_geom'], map(lambda l: rs.coercecurve(l),geom_ref)]]
+    ['stepback_geom', map(lambda l: rs.coercecurve(l),geom_ref)],\
+    ['end_rule']]
+    
+    for i, r in enumerate(rule_):
+        rule.Add(r)
 else:
     rule = []
