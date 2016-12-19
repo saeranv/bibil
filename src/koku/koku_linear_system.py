b9a3fd0d48f349e9694eef47f7198b7bdc0fbf91
Saeran Vasanthakumar <saeranv@gmail.com>
2016-12-18 22:57:31 -0500

    Saving function to compute gaussian elimination
    Also need to rewrite to be more efficeint/straightforward

--- src/koku/koku_linear_system.py
+++ src/koku/koku_linear_system.py
@@ -84,6 +84,41 @@ class LinearSystem(object):
                 else:
                     raise e
         return indices
+    def compute_ge(self):
+        #Takes matrix and outputs gaussian_elimination
+        #(a) Unique solution to matrix as vector
+        #(b) Indication there is no solution
+        #(c) Indiciation there are infinite solutions
+        
+        system = self.compute_rref()
+        # Compute pivot indices, loop through each row,
+        # Check: if unique then add to vector
+        # Check: if parameter present then not unique
+        # Check: if 0 = k then no solution 
+        solution = []
+        pivot_indices = system.indices_of_first_nonzero_terms_in_each_row()
+        pivot_indices
+        for i in range(len(system))[::-1]:
+            j = pivot_indices[i]
+            constant_term = system[i].constant_term
+            if j < 0 and not MyDecimal(constant_term).is_near_zero():
+                # 0 = k, no solution
+                print 'no solution'
+                solution = []
+                break
+            else:
+                # Check if there are other zeros
+                if j+1 < system.dimension-1: 
+                    sum_coord = reduce(lambda c,d:c+d,system[i].normal_vector[j+1:])
+                    sum_coord = Decimal(str(sum_coord))
+                    if not MyDecimal(sum_coord).is_near_zero():
+                        #parameter
+                        print 'solution is paramaterized'
+                solution.append(system[i].constant_term)
+        if solution:
+            solution.reverse()
+            solution = Vector(solution)
+        return solution
     def compute_rref(self):
         #RREF:
         #1. Triangular form
@@ -136,6 +171,7 @@ class LinearSystem(object):
             
         #print '---'
         #print system
+        
         return system
     def compute_triangular_form(self):
         # Compute triangular form, i.e
@@ -220,57 +256,33 @@ class MyDecimal(Decimal):
     def is_near_zero(self, eps=1E-10):
         return abs(float(self)) < eps
 
+#Test 5: Gaussian Elimination
+p1 = Plane(normal_vector=Vector(['5.862','1.178','-10.366']), constant_term='-8.15')
+p2 = Plane(normal_vector=Vector(['-2.931','-0.589','5.183']), constant_term='-4.075')
+s = LinearSystem([p1,p2])
+#print s.compute_rref()
+#sol = s.compute_ge()
+#print sol
 
-L = range(1,10)
-print L[::-1]
-print L.reverse()
-print L
-
-#Test 4: RREF
-
-p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
-p2 = Plane(normal_vector=Vector(['0','1','0']), constant_term='2')
-p3 = Plane(normal_vector=Vector(['0','0','0']), constant_term='0')
+p1 = Plane(normal_vector=Vector(['8.631','5.112','-1.816']), constant_term='-5.113')
+p2 = Plane(normal_vector=Vector(['4.315','11.132','-5.27']), constant_term='-6.775')
+p3 = Plane(normal_vector=Vector(['-2.158','3.01','-1.727']), constant_term='-0.831')
 s = LinearSystem([p1,p2,p3])
-a = s.compute_rref()
+print s.compute_rref()
+sol = s.compute_ge()
+print sol
+print '---'
 
-p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
-p2 = Plane(normal_vector=Vector(['0','1','1']), constant_term='2')
-s = LinearSystem([p1,p2])
-r = s.compute_rref()
-if not (r[0] == Plane(normal_vector=Vector(['1','0','0']), constant_term='-1') and
-        r[1] == p2):
-    print 'test case 1 failed'
-
-p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
-p2 = Plane(normal_vector=Vector(['1','1','1']), constant_term='2')
-s = LinearSystem([p1,p2])
-r = s.compute_rref()
-if not (r[0] == p1 and
-        r[1] == Plane(constant_term='1')):
-    print 'test case 2 failed'
-
-p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
-p2 = Plane(normal_vector=Vector(['0','1','0']), constant_term='2')
-p3 = Plane(normal_vector=Vector(['1','1','-1']), constant_term='3')
-p4 = Plane(normal_vector=Vector(['1','0','-2']), constant_term='2')
+p1 = Plane(normal_vector=Vector(['5.262','2.739','-9.878']), constant_term='-3.441')
+p2 = Plane(normal_vector=Vector(['5.111','6.358','7.638']), constant_term='-2.152')
+p3 = Plane(normal_vector=Vector(['2.016','-9.924','-1.367']), constant_term='-9.278')
+p4 = Plane(normal_vector=Vector(['2.167','-13.543','-18.883']), constant_term='-10.567')
 s = LinearSystem([p1,p2,p3,p4])
-r = s.compute_rref()
-if not (r[0] == Plane(normal_vector=Vector(['1','0','0']), constant_term='0') and
-        r[1] == p2 and
-        r[2] == Plane(normal_vector=Vector(['0','0','-2']), constant_term='2') and
-        r[3] == Plane()):
-    print 'test case 3 failed'
+#print s.compute_rref()
+#sol = s.compute_ge()
+#print sol
+#print '---'
 
-p1 = Plane(normal_vector=Vector(['0','1','1']), constant_term='1')
-p2 = Plane(normal_vector=Vector(['1','-1','1']), constant_term='2')
-p3 = Plane(normal_vector=Vector(['1','2','-5']), constant_term='3')
-s = LinearSystem([p1,p2,p3])
-r = s.compute_rref()
-if not (r[0] == Plane(normal_vector=Vector(['1','0','0']), constant_term=Decimal('23')/Decimal('9')) and
-        r[1] == Plane(normal_vector=Vector(['0','1','0']), constant_term=Decimal('7')/Decimal('9')) and
-        r[2] == Plane(normal_vector=Vector(['0','0','1']), constant_term=Decimal('2')/Decimal('9'))):
-    print 'test case 4 failed'
 
 
 ## Test 0
@@ -401,4 +413,50 @@ if not (t[0] == Plane(normal_vector=Vector(['1','-1','1']), constant_term='2') a
         t[2] == Plane(normal_vector=Vector(['0','0','-9']), constant_term='-2')):
     print 'test case 4 failed'
 """
+"""
+#Test 4: RREF
+p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['0','1','0']), constant_term='2')
+p3 = Plane(normal_vector=Vector(['0','0','0']), constant_term='0')
+s = LinearSystem([p1,p2,p3])
+a = s.compute_rref()
 
+p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['0','1','1']), constant_term='2')
+s = LinearSystem([p1,p2])
+r = s.compute_rref()
+if not (r[0] == Plane(normal_vector=Vector(['1','0','0']), constant_term='-1') and
+        r[1] == p2):
+    print 'test case 1 failed'
+
+p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['1','1','1']), constant_term='2')
+s = LinearSystem([p1,p2])
+r = s.compute_rref()
+if not (r[0] == p1 and
+        r[1] == Plane(constant_term='1')):
+    print 'test case 2 failed'
+
+p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['0','1','0']), constant_term='2')
+p3 = Plane(normal_vector=Vector(['1','1','-1']), constant_term='3')
+p4 = Plane(normal_vector=Vector(['1','0','-2']), constant_term='2')
+s = LinearSystem([p1,p2,p3,p4])
+r = s.compute_rref()
+if not (r[0] == Plane(normal_vector=Vector(['1','0','0']), constant_term='0') and
+        r[1] == p2 and
+        r[2] == Plane(normal_vector=Vector(['0','0','-2']), constant_term='2') and
+        r[3] == Plane()):
+    print 'test case 3 failed'
+
+p1 = Plane(normal_vector=Vector(['0','1','1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['1','-1','1']), constant_term='2')
+p3 = Plane(normal_vector=Vector(['1','2','-5']), constant_term='3')
+s = LinearSystem([p1,p2,p3])
+r = s.compute_rref()
+if not (r[0] == Plane(normal_vector=Vector(['1','0','0']), constant_term=Decimal('23')/Decimal('9')) and
+        r[1] == Plane(normal_vector=Vector(['0','1','0']), constant_term=Decimal('7')/Decimal('9')) and
+        r[2] == Plane(normal_vector=Vector(['0','0','1']), constant_term=Decimal('2')/Decimal('9'))):
+    print 'test case 4 failed'
+
+"""
